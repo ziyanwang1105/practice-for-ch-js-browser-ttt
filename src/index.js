f1d58e8fcce8be742282ea4b85bcc12b0a4aa348
ChristopherJTrent <ChristopherJTrent@outlook.com>
2024-03-05 13:50:46 -0800

    implement basic markup generation

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
-Add your import statements for View and Game here
+//Add your import statements for View and Game here
+import View from "./ttt-view";
+const Game = require("../ttt_node/game")
 
 document.addEventListener("DOMContentLoaded", () => {
   // Your code here
+  const v = new View(new Game(), document.querySelector('.ttt'));
 });
--- src/ttt-view.js
+++ src/ttt-view.js
@@ -1,17 +1,40 @@
 class View {
+  /**
+   * 
+   * @param {Game} game 
+   * @param {HTMLElement} el <figure class="ttt">
+   */
   constructor(game, el) {
+    this.game = game
+    el.appendChild(this.setupBoard());
   }
   
   setupBoard() {
+    //<li data-pos="[0,0]"></li>
+    let ul = document.createElement("ul")
+    for (let i = 0; i < 9; i ++) {
+      let li = document.createElement('li')
+      li.setAttribute("data-pos", `[${Math.floor(i / 3)},${i % 3}]`)
+      li.addEventListener('click', this.handleClick.bind(this))
+      ul.appendChild(li);
+
+    }
+    return ul;
   }
-  
+  /**
+   * 
+   * @param {Event} e 
+   */
   handleClick(e) {
+    alert(JSON.parse(e.target.dataset.pos))
   }
 
   makeMove(square) {
+
   }
   
   handleGameOver() {
+
   }
 }
 
